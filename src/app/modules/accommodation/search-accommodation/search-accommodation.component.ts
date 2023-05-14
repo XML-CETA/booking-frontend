391722528ae5c9a324ac30597fb1e9a4a55dd31f
Vasilije Milic <milicvasilije9@gmail.com>
2023-05-14 21:01:51 +0200

    Implemented interface for searching accommodaitons, creating reservations, and reservation list

---
+++ src/app/modules/accommodation/search-accommodation/search-accommodation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-accommodation',
+  templateUrl: './search-accommodation.component.html',
+  styleUrls: ['./search-accommodation.component.css']
+})
+export class SearchAccommodationComponent {
+
+}
