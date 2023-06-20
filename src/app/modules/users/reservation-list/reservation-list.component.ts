7a06fd8ed93b9c93dbd6489373037d4dfac3015e
DavidT <terenji.ra178.2019@uns.ac.rs>
2023-06-20 15:13:14 +0200

    host-rate

--- src/app/modules/users/reservation-list/reservation-list.component.ts
+++ src/app/modules/users/reservation-list/reservation-list.component.ts
@@ -3,6 +3,7 @@ import { ReservationService } from '../service/reservation.service';
 import { Reservation, Status } from './model/Reservation';
 import { AuthService } from '../../auth/service/auth.service';
 import jwtDecode from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-list',
@@ -10,31 +11,49 @@ import jwtDecode from 'jwt-decode';
   styleUrls: ['./reservation-list.component.css']
 })
 export class ReservationListComponent {
-    public reservations: Reservation[] = [];
-    public token: any = localStorage.getItem('token');
-    public user: string = ''
-    constructor(private reservationService:ReservationService, private authService: AuthService) { }
-  
-    ngOnInit(): void {
-      this.user = (<any>jwtDecode(this.token)).custom_claims.email;
-      this.reservationService.getAll().subscribe(data => {
-        this.reservations = data.reservations;
-        this.reservations = this.reservations.filter(reservation => reservation.user == this.user && reservation.status == Status.Reserved );
-      });
+  public reservations: Reservation[] = [];
+  public token: any = localStorage.getItem('token');
+  public user: string = ''
+  constructor(private reservationService: ReservationService, private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.user = (<any>jwtDecode(this.token)).custom_claims.email;
+    this.reservationService.getAll().subscribe(data => {
+      this.reservations = data.reservations;
+      this.reservations = this.reservations.filter(reservation => reservation.user == this.user && reservation.status == Status.Reserved);
+    });
+  }
+
+  cancelReservation(id: string) {
+    this.reservationService.cancel(id).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  checkDate(dateFrom: string) {
+    const currentDate = new Date();
+    const reservationEndDate = new Date(dateFrom);
+    const timeDifference = reservationEndDate.getTime() - currentDate.getTime();
+    const oneDayMilliseconds = 24 * 60 * 60 * 1000;
+    const daysRemaining = Math.floor(timeDifference / oneDayMilliseconds);
+    return daysRemaining >= 1;
+  }
+
+  handleReservationClick(event: Event, dateTo: string, host: string): void {
+    event.preventDefault(); // Prevent default navigation behavior
+
+    if (this.isDatePast(dateTo)) {
+      // Date is in the past, do not perform any action
+      return;
     }
 
-    cancelReservation(id: string){
-      this.reservationService.cancel(id).subscribe(data => {
-        console.log(data);
-      });
-    }
+    // Date is in the future, navigate to the desired route or perform the desired action
+    this.router.navigate(['/host', host, 'rate']);
+  }
 
-    checkDate(dateFrom:string){
-      const currentDate = new Date();
-      const reservationEndDate = new Date(dateFrom);
-      const timeDifference = reservationEndDate.getTime() - currentDate.getTime();
-      const oneDayMilliseconds = 24 * 60 * 60 * 1000;
-      const daysRemaining = Math.floor(timeDifference / oneDayMilliseconds);
-      return daysRemaining >= 1;
-    }
+  isDatePast(date: string): boolean {
+    const currentDate = new Date();
+    const targetDate = new Date(date.replace(/-/g, '/')); // Replace hyphens with slashes for wider date format support
+    return targetDate.getTime() < currentDate.getTime();
+  }
 }
